30a80f76e6d847961e91cb0ccc073c9f0e121a94
Xingyi Wang <jarvis.xyw@gmail.com>
2019-03-21 08:28:27 +0800

    Local vis

---
+++ Visualization/draw_fig.py
@@ -0,0 +1,58 @@
+from PIL import Image
+
+
+# draw input/affected
+def draw_binary(filename, array, save=0):
+    if len(array.shape) == 1:
+        x = 20
+    else:
+        x = 20 * array.shape[1]
+    y = 20 * array.shape[0]
+    im = Image.new("RGB", (x, y))
+    for i in range(x):
+        for j in range(y):
+            array_x = int(i / 20)
+            array_y = int(j / 20)
+            if len(array.shape) == 1:
+                value = array[array_y]
+            else:
+                value = array[array_y][array_x]
+            if value == 1:
+                im.putpixel((i, j), (255, 0, 0))
+            else:
+                im.putpixel((i, j), (255, 255, 255))
+    if save == 0:
+        im.show(filename)
+    else:
+        im.save('fig/' + filename + '.png')
+
+
+# draw figure
+def draw_hidden_states(filename, array, save=0):
+    if len(array.shape) == 1:
+        x = 20
+    else:
+        x = 20 * array.shape[1]
+    y = 20 * array.shape[0]
+    im = Image.new("RGB", (x, y))
+    for i in range(x):
+        for j in range(y):
+            # print i, j
+            array_x = int(i / 20)
+            array_y = int(j / 20)
+            # print array_x, array_y
+            if len(array.shape) == 1:
+                value = array[array_y]
+            else:
+                value = array[array_y][array_x]
+            if value > 0:
+                saturation = int(255 * value)
+                im.putpixel((i, j), (255, 255 - saturation, 255 - saturation))
+            else:
+                saturation = int(-255 * value)
+                im.putpixel((i, j), (255 - saturation, 255 - saturation, 255))
+        # raw_input()
+    if save == 0:
+        im.show(filename)
+    else:
+        im.save('fig/' + filename + '.png')
---
+++ Visualization/main.py
@@ -0,0 +1,114 @@
+import h5py
+import numpy as np
+from draw_fig import draw_binary
+from draw_fig import draw_hidden_states
+
+CYCLE = 50
+
+# file_data = open('inputdata_b10', 'r')  # 500250
+# file_error = open('inputerror_b10', 'r')  # 500250
+# file_flipped = open('flipped_b10', 'r')  # 10005
+# file_affected = open('affected_b10', 'r')  # 500250
+# file_weights = h5py.File('weights_b10.hdf5', 'r')
+file_h_states_J = h5py.File('hidden_states_J_f.hdf5', 'r')
+# file_h_states_J_free = h5py.File('hidden_states_J_e_free.hdf5', 'r')
+
+'''
+# ---------- flipped for inputerror ----------
+print('processing flipped...')
+flipped_raw = np.loadtxt('flipped_b10_e', dtype='int64')
+# permanent fault
+#flipped = flipped_raw.copy()
+# intermittent fault
+flipped_raw = flipped_raw.reshape((-1, CYCLE, flipped_raw.shape[1]))
+print(flipped_raw.shape)
+flipped = np.zeros((flipped_raw.shape[0], flipped_raw.shape[1]-1, flipped_raw.shape[2]), dtype='int64')
+for i in range(flipped.shape[0]):
+    flipped[i] = np.delete(flipped_raw[i], 0, axis=0)
+'''
+
+# hidden states J
+print(file_h_states_J.keys())  # input, affected, hidden_states
+input_array = file_h_states_J['input']
+affected_array = file_h_states_J['affected']
+hidden_states_array = file_h_states_J['hidden_states']
+rightORwrong_array = file_h_states_J['rightORwrong']
+
+# hidden states J free
+'''
+print file_h_states_J_free.keys() # input, affected, hidden_states
+input_array = file_h_states_J_free['input']
+hidden_states_array = file_h_states_J_free['hidden_states']
+'''
+
+start_i = 0
+i = 0
+for (i_key, a_key, h_key, rw_key) in \
+        zip(input_array.keys(), affected_array.keys(), hidden_states_array.keys(), rightORwrong_array.keys()):
+    if i < start_i:
+        i += 1
+        continue
+    print('Sample: ', i)
+    # Input
+    # print(input_array[i_key].name)
+    # print(input_array[i_key][()])
+    draw_binary('input_%d' % i, input_array[i_key][()].T, save=1)
+    # Flipped
+    # Affected
+    # print(affected_array[a_key].name)
+    # print(affected_array[a_key][()])
+    draw_binary('affected_%d' % i, affected_array[a_key][()].T, save=1)
+    # Hidden states
+    # print(hidden_states_array[h_key].name)
+    # print(hidden_states_array[h_key][()])
+    draw_hidden_states('hidden_states_%d' % i, hidden_states_array[h_key][()].T, save=1)
+    # Right or Wrong
+    # print(rightORwrong_array[rw_key].name)
+    print(rightORwrong_array[rw_key][()])
+    if rightORwrong_array[rw_key][()] == 1:
+        print('right')
+    else:
+        print('wrong')
+    # draw_pixel()
+    i += 1
+    input()
+
+'''
+# ---------- LSTM weights ----------
+print file_weights.keys()
+model_weights = file_weights['model_weights'] # dense_1, dropout_1, lstm_1
+optimizer_weights = file_weights['optimizer_weights'] # Adam, training
+
+# optimizer_weights
+print optimizer_weights['training']['Adam']['Variable:0'] # (3, 32)
+
+print optimizer_weights['training']['Adam']['Variable_1:0'] # (8, 32)
+print optimizer_weights['training']['Adam']['Variable_2:0'] # (32,)
+print optimizer_weights['training']['Adam']['Variable_3:0'] # (8, 1)
+print optimizer_weights['training']['Adam']['Variable_4:0'] # (1,)
+print optimizer_weights['training']['Adam']['Variable_5:0'] # (3, 32)
+print optimizer_weights['training']['Adam']['Variable_6:0'] # (8, 32)
+print optimizer_weights['training']['Adam']['Variable_7:0'] # (32,)
+print optimizer_weights['training']['Adam']['Variable_8:0'] # (8, 1)
+print optimizer_weights['training']['Adam']['Variable_9:0'] # (1,)
+
+# model_weights
+dense_1 = model_weights['dense_1']['dense_1'] # bias:0, kernel:0
+lstm_1 = model_weights['lstm_1']['lstm_1'] # bias:0, kernel:0, recurrent_kernel:0
+lstm_kernel = lstm_1['kernel:0']
+lstm_recurrent_kernel = lstm_1['recurrent_kernel:0']
+lstm_bias = lstm_1['bias:0']
+
+lstm_kernel_array = lstm_kernel[()].T
+lstm_recurrent_kernel_array = lstm_recurrent_kernel[()].T
+lstm_bias_array = lstm_bias[()].T
+print lstm_kernel
+print lstm_recurrent_kernel
+print lstm_bias
+
+#f_out = h5py.File('states.hdf5','w')
+#f_out.create_dataset('lstm_1_recurrent_kernel', data=grp1['lstm_1']['lstm_1']['recurrent_kernel'])
+#f_out.create_dataset('lstm_1_kernel', data=grp1['lstm_1']['lstm_1']['kernel'])
+#f_out.create_dataset('lstm_1_bias', data=grp1['lstm_1']['lstm_1']['bias'])
+#f_out.close()
+'''
