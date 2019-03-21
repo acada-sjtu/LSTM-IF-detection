294bd7c9cce70b8d4b031ec73fc87d3f2c7366bf
Xingyi Wang <jarvis.xyw@gmail.com>
2019-03-21 17:25:09 +0800

    Add files via upload

--- Visualization/main.py
+++ Visualization/main.py
@@ -1,45 +1,51 @@
+import sys
 import h5py
 import numpy as np
 from draw_fig import draw_binary
 from draw_fig import draw_hidden_states
+from draw_fig import draw_bi_hidden_states
 
 CYCLE = 50
 
-# file_data = open('inputdata_b10', 'r')  # 500250
-# file_error = open('inputerror_b10', 'r')  # 500250
-# file_flipped = open('flipped_b10', 'r')  # 10005
-# file_affected = open('affected_b10', 'r')  # 500250
-# file_weights = h5py.File('weights_b10.hdf5', 'r')
-file_h_states_J = h5py.File('hidden_states_J_f.hdf5', 'r')
-# file_h_states_J_free = h5py.File('hidden_states_J_e_free.hdf5', 'r')
+file_path = sys.argv[1]
+if file_path.split('/')[1].split('_')[0]=='bi':
+	bi_h_states_flag = 1
+else:
+	bi_h_states_flag = 0
 
-'''
-# ---------- flipped for inputerror ----------
-print('processing flipped...')
-flipped_raw = np.loadtxt('flipped_b10_e', dtype='int64')
-# permanent fault
-#flipped = flipped_raw.copy()
-# intermittent fault
-flipped_raw = flipped_raw.reshape((-1, CYCLE, flipped_raw.shape[1]))
-print(flipped_raw.shape)
-flipped = np.zeros((flipped_raw.shape[0], flipped_raw.shape[1]-1, flipped_raw.shape[2]), dtype='int64')
-for i in range(flipped.shape[0]):
-    flipped[i] = np.delete(flipped_raw[i], 0, axis=0)
-'''
+file_h_states_J = h5py.File(file_path, 'r')
+
+
+# # ---------- flipped for inputerror ----------
+# print('processing flipped...')
+# flipped_raw = np.loadtxt('flipped_b10_e', dtype='int64')
+# # permanent fault
+# flipped = flipped_raw.copy()
+# # intermittent fault
+# flipped_raw = flipped_raw.reshape((-1, CYCLE, flipped_raw.shape[1]))
+# print(flipped_raw.shape)
+# flipped = np.zeros((flipped_raw.shape[0], flipped_raw.shape[1]-1, flipped_raw.shape[2]), dtype='int64')
+# for i in range(flipped.shape[0]):
+#     flipped[i] = np.delete(flipped_raw[i], 0, axis=0)
+
 
 # hidden states J
 print(file_h_states_J.keys())  # input, affected, hidden_states
 input_array = file_h_states_J['input']
 affected_array = file_h_states_J['affected']
-hidden_states_array = file_h_states_J['hidden_states']
+if bi_h_states_flag==1:
+	hidden_states_array = file_h_states_J['bi_hidden_states']
+else:
+	hidden_states_array = file_h_states_J['hidden_states']
+
 rightORwrong_array = file_h_states_J['rightORwrong']
 
-# hidden states J free
-'''
-print file_h_states_J_free.keys() # input, affected, hidden_states
-input_array = file_h_states_J_free['input']
-hidden_states_array = file_h_states_J_free['hidden_states']
-'''
+# # hidden states J free
+# print(file_h_states_J.keys())  # input, hidden_states
+# input_array = file_h_states_J_free['input']
+# hidden_states_array = file_h_states_J_free['hidden_states']
+# rightORwrong_array = file_h_states_J['rightORwrong']
+
 
 start_i = 0
 i = 0
@@ -61,7 +67,10 @@ for (i_key, a_key, h_key, rw_key) in \
     # Hidden states
     # print(hidden_states_array[h_key].name)
     # print(hidden_states_array[h_key][()])
-    draw_hidden_states('hidden_states_%d' % i, hidden_states_array[h_key][()].T, save=1)
+    if bi_h_states_flag==1:
+    	draw_bi_hidden_states('bi_hidden_states_%d' % i, hidden_states_array[h_key][()].T, save=1)
+    else:
+    	draw_hidden_states('hidden_states_%d' % i, hidden_states_array[h_key][()].T, save=1)
     # Right or Wrong
     # print(rightORwrong_array[rw_key].name)
     print(rightORwrong_array[rw_key][()])
