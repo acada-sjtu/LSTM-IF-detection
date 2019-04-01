a9efc149b4cbd794f32102d5ad1d7f6f2f26f4f8
Xingyi Wang <jarvis.xyw@gmail.com>
2019-04-01 10:20:00 +0800

    Add files via upload

--- standard/read_line_num.py
+++ standard/read_line_num.py
@@ -1,6 +1,9 @@
 import sys
+CYCLE = 50
 
 filename = sys.argv[1]
 file = open(filename)
 lines = len(file.readlines())
-print lines
+print '# of lines:', lines
+print '# of cycles:', CYCLE
+print '# of samples:', int(lines / CYCLE)
